606bb9da8463ed2fa4929f2eaaebdf4c2ad9bc3e
Renan <rgmelo94@gmail.com>
2018-05-17 13:48:52 -0300

    add https

--- src/plugins/uploadImagem.js
+++ src/plugins/uploadImagem.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const pathUrlLocal = 'http://localhost:8000/api/v1/foto';
-const pathUrlProd = 'http://api-chat-aps.herokuapp.com/api/v1/foto';
+const pathUrlProd = 'https://api-chat-aps.herokuapp.com/api/v1/foto';
 export default ({ app, router, Vue }) => {
   Vue.prototype.$uploadImagem = (img) => {
     const data = {
